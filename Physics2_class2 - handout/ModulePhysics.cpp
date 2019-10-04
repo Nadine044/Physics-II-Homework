dd8cc2834f1e9f9d4d3ac943283377e1ff090755
Nadine Gutiérrez Pociño <nadinegp@TR12.CAT>
2019-10-04 11:54:46 +0200

    2nd Handout
    Need to be finished

---
+++ Physics2_class2 - handout/ModulePhysics.cpp
@@ -0,0 +1,265 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleInput.h"
+#include "ModuleRender.h"
+#include "ModulePhysics.h"
+#include "math.h"
+
+#include "Box2D/Box2D/Box2D.h"
+
+#ifdef _DEBUG
+#pragma comment( lib, "Box2D/libx86/Debug/Box2D.lib" )
+#else
+#pragma comment( lib, "Box2D/libx86/Release/Box2D.lib" )
+#endif
+
+ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	world = NULL;
+	debug = true;
+}
+
+// Destructor
+ModulePhysics::~ModulePhysics()
+{
+}
+
+bool ModulePhysics::Start()
+{
+	LOG("Creating Physics 2D environment");
+
+	world = new b2World(b2Vec2(GRAVITY_X, -GRAVITY_Y));
+
+	// big static circle as "ground" in the middle of the screen
+	int x = SCREEN_WIDTH / 2;
+	int y = SCREEN_HEIGHT / 1.5f;
+	int diameter = SCREEN_WIDTH / 2;
+
+	b2BodyDef body;
+	body.type = b2_staticBody;
+	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* b = world->CreateBody(&body);
+
+	b2CircleShape shape;
+	shape.m_radius = PIXEL_TO_METERS(diameter) * 0.5f;
+
+	b2FixtureDef fixture;
+	fixture.shape = &shape;
+	b->CreateFixture(&fixture);
+
+	return true;
+}
+
+// 
+update_status ModulePhysics::PreUpdate()
+{
+	world->Step(1.0f / 60.0f, 6, 2);
+
+	return UPDATE_CONTINUE;
+}
+
+// 
+update_status ModulePhysics::PostUpdate()
+{
+	// On space bar press, create a circle on mouse position
+	if(App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
+	{
+		b2BodyDef body;
+		body.type = b2_dynamicBody;
+		float radius = PIXEL_TO_METERS(25);
+		body.position.Set(PIXEL_TO_METERS(App->input->GetMouseX()), PIXEL_TO_METERS(App->input->GetMouseY()));
+
+		b2Body* b = world->CreateBody(&body);
+
+		b2CircleShape shape;
+		shape.m_radius = radius;
+		b2FixtureDef fixture;
+		fixture.shape = &shape;
+
+		b->CreateFixture(&fixture);
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
+	{
+		// TODO 1: When pressing 2, create a box on the mouse position
+
+		b2BodyDef boxBody;
+		boxBody.type = b2_dynamicBody;
+
+		boxBody.position.Set(PIXEL_TO_METERS(App->input->GetMouseX()), PIXEL_TO_METERS(App->input->GetMouseY()));
+
+		b2Body* b = world->CreateBody(&boxBody);
+
+		b2PolygonShape shape;
+		shape.SetAsBox(PIXEL_TO_METERS(20), PIXEL_TO_METERS(20));
+		b2FixtureDef fixture;
+
+		// TODO 2: To have the box behave normally, set fixture's density to 1.0f
+
+		fixture.density = 1.0f;
+		fixture.shape = &shape;
+
+		b->CreateFixture(&fixture);
+
+		
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN)
+	{
+		// TODO 3: Create a chain shape using those vertices
+		// remember to convert them from pixels to meters!
+
+		b2Vec2 vec[34];		
+
+		// Pivot 0, 0
+		int rick_head[68] = {
+			43, 39,
+			40, 0,
+			75, 32,
+			88, 4,
+			94, 40,
+			112, 34,
+			103, 58,
+			107, 63,
+			117, 66,
+			108, 74,
+			109, 80,
+			105, 93,
+			110, 98,
+			102, 104,
+			99, 116,
+			105, 121,
+			104, 126,
+			98, 126,
+			93, 140,
+			85, 145,
+			69, 148,
+			56, 143,
+			46, 132,
+			34, 137,
+			39, 126,
+			23, 125,
+			32, 114,
+			9, 102,
+			29, 90,
+			0, 76,
+			30, 62,
+			13, 36,
+			22, 37,
+			25, 42
+		};
+
+		int j = 0;
+		for (int i = 0; i < 34; i++)
+		{
+			vec[i].Set(PIXEL_TO_METERS(rick_head[j]), PIXEL_TO_METERS(rick_head[j + 1]));
+			j += 2;
+		}
+
+		b2BodyDef chainBody;
+		chainBody.type = b2_dynamicBody;
+		chainBody.position.Set(PIXEL_TO_METERS(App->input->GetMouseX()), PIXEL_TO_METERS(App->input->GetMouseY()));
+
+		b2Body* b = world->CreateBody(&chainBody);
+
+		b2ChainShape shape;
+		shape.CreateLoop(vec, 34);
+		b2FixtureDef fixture;
+		fixture.shape = &shape;
+
+		b->CreateFixture(&fixture);		
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
+		debug = !debug;
+
+	if(!debug)
+		return UPDATE_CONTINUE;
+
+	// Bonus code: this will iterate all objects in the world and draw the circles
+	// You need to provide your own macro to translate meters to pixels
+	for(b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+	{
+		for(b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
+		{
+			switch(f->GetType())
+			{
+				// Draw circles ------------------------------------------------
+				case b2Shape::e_circle:
+				{
+					b2CircleShape* shape = (b2CircleShape*)f->GetShape();
+					b2Vec2 pos = f->GetBody()->GetPosition();
+					App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), METERS_TO_PIXELS(shape->m_radius), 255, 255, 255);
+				}
+				break;
+
+				// Draw polygons ------------------------------------------------
+				case b2Shape::e_polygon:
+				{
+					b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
+					int32 count = polygonShape->GetVertexCount();
+					b2Vec2 prev, v;
+
+					for(int32 i = 0; i < count; ++i)
+					{
+						v = b->GetWorldPoint(polygonShape->GetVertex(i));
+						if(i > 0)
+							App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 255, 100, 100);
+
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(polygonShape->GetVertex(0));
+					App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 255, 100, 100);
+				}
+				break;
+
+				// Draw chains contour -------------------------------------------
+				case b2Shape::e_chain:
+				{
+					b2ChainShape* shape = (b2ChainShape*)f->GetShape();
+					b2Vec2 prev, v;
+
+					for(int32 i = 0; i < shape->m_count; ++i)
+					{
+						v = b->GetWorldPoint(shape->m_vertices[i]);
+						if(i > 0)
+							App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 100, 255, 100);
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(shape->m_vertices[0]);
+					App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 100, 255, 100);
+				}
+				break;
+
+				// Draw a single segment(edge) ----------------------------------
+				case b2Shape::e_edge:
+				{
+					b2EdgeShape* shape = (b2EdgeShape*)f->GetShape();
+					b2Vec2 v1, v2;
+
+					v1 = b->GetWorldPoint(shape->m_vertex0);
+					v1 = b->GetWorldPoint(shape->m_vertex1);
+					App->renderer->DrawLine(METERS_TO_PIXELS(v1.x), METERS_TO_PIXELS(v1.y), METERS_TO_PIXELS(v2.x), METERS_TO_PIXELS(v2.y), 100, 100, 255);
+				}
+				break;
+			}
+		}
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+
+// Called before quitting
+bool ModulePhysics::CleanUp()
+{
+	LOG("Destroying physics world");
+
+	// Delete the whole physics world!
+	delete world;
+
+	return true;
+}
